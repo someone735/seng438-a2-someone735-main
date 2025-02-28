e8b1d9fae63b4e2e9f3be576ee1f68ebd19cb3fc
someone735 <smeone83@gmail.com>
2025-02-27 21:24:06 -0700

    adding Junit Test Suite

---
+++ JFreeChart/src/org/jfree/data/test/CombineTest.java
@@ -0,0 +1,32 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*; 
+import org.jfree.data.Range; 
+import org.junit.*;
+
+
+public class CombineTest {
+	private Range exampleRangeOne;
+	private Range exampleRangeTwo;
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	public void similarRanges() {
+		fail("Not yet implemented");
+	}
+	@Test
+	public void exactRanges() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void differentRanges(){
+		fail("Not yet implemented");
+	}
+	
+
+
+}
---
+++ JFreeChart/src/org/jfree/data/test/createNumberAray2DTest.java
@@ -0,0 +1,16 @@
+package org.jfree.data.test;
+
+import static org.junit.Assert.*;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.*;
+import org.junit.*;
+
+public class createNumberAray2DTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
